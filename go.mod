1b04c212e4bebeb9137587468ec64587bbb059b8
zakiego <belummikir@gmail.com>
2022-04-28 16:09:41 +0800

    feat: add windows tih cron

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/zakiego/isalive
 
 go 1.18
 
+require github.com/robfig/cron/v3 v3.0.1
--- main.go
+++ main.go
@@ -5,22 +5,92 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+	"time"
+
+	cron "github.com/robfig/cron/v3"
 )
-
-
 
 func main() {
 	os := runtime.GOOS
 
 	switch os {
 	case "windows":
-		fmt.Println("Windows")
+		CronWindows()
 	case "linux":
 		ubuntu()
 	}
+}
+
+func CronWindows() {
+	c := cron.New()
+	// i := 1
+	c.AddFunc("* * * * *", func() {
+		Windows()
+	})
+	c.Start()
+	time.Sleep(time.Minute * 5)
+}
+
+func Windows() {
+
+	file, _ := os.Create(`F:\Code\go\isalive-app\log.txt`)
+
+	// get list run application
+	cmd := exec.Command(`F:\Code\go\isalive-app\wmctrl.exe`, "-c", "-l")
+
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// print list run application
+	// trim space and join again
+	var output string
+	for id, line := range strings.Split(strings.TrimRight(string(stdout), "\n"), "\n") {
+		if id < 3 {
+			continue
+		}
+
+		words := strings.Fields(line)
+		window := words[1] + " - " + strings.Join(words[2:], " ")
+
+		if id == 3 {
+			output = window
+			continue
+		}
+		output = output + "\n" + window
+	}
+
+	file.WriteString(output)
+	file.Close()
+
+	link := "https://api-isalive.zakiego.workers.dev/?device=laptop-windows&status="
+	api := fmt.Sprint(link, url.QueryEscape(output))
+
+	// get api
+	resp, err := http.Get(string(api))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// read response
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(body))
+
 }
 
 func ubuntu() {
